ff53b6c9dfadc56d975f7c09a478c8b6ff066627
SongNan <nan.song@thoughtworks.com>
2020-05-26 13:41:53 +0800

    [Song] feat: rewrite element ui theme.

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import ElementUI from 'element-ui';
 import App from './App.vue';
 import router from './router';
-import 'element-ui/lib/theme-chalk/index.css';
+import './common/styles/element-variables.scss';
 
 Vue.config.productionTip = false;
 
