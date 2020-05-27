6644e6ec62ec444f3c084e1e1bcae420baeb4e96
SongNan <nan.song@thoughtworks.com>
2020-05-27 13:33:52 +0800

    [Song] feat: add login & signUp btn.

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,16 @@
 <template>
   <header>
     <div id="app-header">
-      <div class="nav-wrapper">
-        <router-link class="logo" to="/"><span>HeroStories</span></router-link>
-        <el-menu router :default-active="$route.path" mode="horizontal" class="app-nav">
+      <div class="content">
+        <router-link class="logo vertical-center" to="/"><span>HeroStories</span></router-link>
+        <el-menu class="app-nav vertical-center"
+                 router :default-active="$route.path" mode="horizontal">
           <el-menu-item class="nav-item" index="/stories">英雄事迹</el-menu-item>
         </el-menu>
+        <div class="btn-wrapper vertical-center" v-if="true">
+          <el-button round type="primary">登陆</el-button>
+          <el-button round >注册</el-button>
+        </div>
       </div>
     </div>
     <div class="header-placeholder"></div>
@@ -22,11 +27,41 @@ export default {
   @import "../common/styles/colors";
 
   $header-height: 70px;
+  $margin: 32px;
 
   header {
     width: 100%;
     text-align: left;
     background-color: white;
+  }
+
+  .content {
+    height: 100%;
+    min-width: 1200px;
+    max-width: 1400px;
+    margin: 0 auto;
+
+    .logo {
+      color: $theme;
+      margin-right: $margin;
+      font-size: 36px;
+      font-weight: bold;
+      text-decoration-line: none;
+      float: left;
+    }
+
+    .app-nav {
+      border-bottom: none;
+      margin-right: $margin;
+
+      .nav-item {
+        height: 50px;
+      }
+    }
+
+    .btn-wrapper {
+      float: right;
+    }
   }
 
   #app-header {
@@ -35,40 +70,20 @@ export default {
     top: 0;
     right: 0;
     z-index: 999;
-    padding: 0 40px;
-    box-shadow: 0 0 1px 1px #00000019;
+    box-shadow: 0 0 1px 1px $shadow;
     width: 100%;
-    box-sizing: border-box;
-  }
-
-  .nav-wrapper {
-    display: flex;
-    align-items: center;
-    height: 100%;
-
-    .logo {
-      color: $theme;
-      display: inline-block;
-      margin-right: 32px;
-      font-size: 36px;
-      font-weight: bold;
-      text-decoration-line: none;
-    }
-
-    .app-nav {
-      display: inline-block;
-      border-bottom: none;
-
-      .nav-item {
-        height: 50px;
-      }
-    }
   }
 
   .header-placeholder {
     width: 100%;
     position: relative;
     height: $header-height;
+  }
+
+  .vertical-center {
+    height: 100%;
+    display: inline-flex;
+    align-items: center;
   }
 
 </style>
