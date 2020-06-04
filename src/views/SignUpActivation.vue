38f28b2258db6f061968fdac96b45651d877b5ab
SongNan <nan.song@thoughtworks.com>
2020-06-04 10:28:45 +0800

    [Song] feat: add countDown view.

--- src/views/SignUpActivation.vue
+++ src/views/SignUpActivation.vue
@@ -1,9 +1,38 @@
 <template>
-  <h1>激活页面</h1>
+  <div id="sign-up-activation">
+    <h2>激活成功！</h2>
+    <h3>{{time}}s 后跳转至<router-link :to="homePath">网站主页</router-link>...</h3>
+  </div>
 </template>
 
 <script>
-export default { name: 'SignUpActivation' };
+export default {
+  name: 'SignUpActivation',
+  data() {
+    return {
+      time: 5,
+      interval: null,
+      homePath: '/',
+    };
+  },
+  methods: {
+    countDown() {
+      this.interval = setInterval(() => {
+        this.time -= 1;
+        if (this.time === 0) {
+          this.$router.push({ path: this.homePath });
+          clearInterval(this.interval);
+        }
+      }, 1000);
+    },
+  },
+  mounted() {
+    this.countDown();
+  },
+  destroyed() {
+    clearInterval(this.interval);
+  },
+};
 </script>
 
 <style scoped>
