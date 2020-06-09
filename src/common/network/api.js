6afe98599bdae4d33faf62be4959b2c476cc4f0b
vickerx <vicker23@163.com>
2020-06-09 17:00:13 +0800

    Add sign in & vue.config.js

--- src/common/network/api.js
+++ src/common/network/api.js
@@ -18,8 +18,11 @@ export const getStories = (page, size) => api.get(`${CORE_URL}/api/hero-story?pa
 
 export const registerUser = (userInfo) => api.post(`${AUTH_URL}/user/registration`, null, { params: userInfo });
 
+<<<<<<< HEAD
 export const activeUser = (code) => api.get(`${AUTH_URL}/user/activate`, { params: { code } });
 
+=======
+>>>>>>> Add sign in & vue.config.js
 export const login = (email, password) => api.post(`${GATEWAY}/login?email=${email}&password=${password}`);
 
 export const logout = () => api.post(`${GATEWAY}/logout`);
